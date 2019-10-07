605d623e26834ed5ed9abca84c459e96fd5da160
Vinicius Mangueira <32227073+ViniciusDeep@users.noreply.github.com>
2019-10-07 15:50:34 -0300

    Tips to possible change

--- RickNMorty/Helpers/NetworkManager.swift
+++ RickNMorty/Helpers/NetworkManager.swift
@@ -7,12 +7,12 @@
 //
 
 import Foundation
-
+// Use netwowk manager as Struct, always choose to value type
 class NetworkManager {
-    
+    // Singleton many times it's not a best solution
     static let sharedInstance = NetworkManager()
     private init() {}
-    
+    // Get is default, you don't need create a URLRequest to just made a Get
     func createGetRequest(url: String) -> URLRequest {
         let getURL = URL(string: url)!
         var getRequest = URLRequest(url: getURL, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 30)
@@ -20,22 +20,25 @@ class NetworkManager {
         return getRequest
     }
     
+    // Just trying use Result of Swift 5, the closure just can be escaping, T or Error, result is enum perfect to made that
     func sendGetRequest<T:Codable>(getRequest: URLRequest, type: T.Type, completion: @escaping (_ result: T?,_ error: Error?) -> Void) {
+       // This code of fetch data is always repeating, just made better, protocol, enum that has implementation to get,post,put,delete,patch use this
+        
         let getTask = URLSession.shared.dataTask(with: getRequest) { (data, response, err) in
             guard err == nil else {
                 print("GET Request in \(getRequest) Error: \(String(describing: err))")
                 return
             }
-            
+            // You can made guard let to data, to escaping Network Error
             if let receivedData = data {
                 do {
                     let resultObject = try JSONDecoder().decode(type, from: receivedData)
                     DispatchQueue.main.async {
-                        completion(resultObject, nil)
+                        completion(resultObject, nil) // Result 
                     }
                 } catch {
                     DispatchQueue.main.async {
-                        completion(nil, error)
+                        completion(nil, error) // Completion(.failure(let error))
                     }
                 }
             } else {
